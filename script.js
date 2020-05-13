b8d247d7e84bd3ce6ad444895badf43db7423e8f
Adriel <adril_trigger@yahoo.com>
2020-05-13 16:23:41 -0300

    rendering and functional individual remove buttons added, as well as shelf limitation for up to 10 books

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 let newBook = document.querySelector('.add-book');
 let form = document.querySelector('.book-data');
+let shelf = document.querySelector('.shelf');
 
 // Book object constructor
 function Book(title,pages,author,status) {
@@ -8,24 +9,62 @@ function Book(title,pages,author,status) {
     this.pages = pages;
     this.author = author;
     this.status = status;
+    this.position;
 }
 
 //User input processing mechanism
 function addBook(title,pages,author,status) {
     let book = new Book(title,pages,author,status);
+    book.position = myLibrary.length;
     myLibrary.push(book);
-    console.log(myLibrary);
+    render(book,book.position);
+}
+
+function removeBook(library,position) {
+    library.splice(position,1);
+    console.log(library);
+}
+
+function render(book,position) {
+    let bookData = document.createElement('div');
+    bookData.classList.add('book');
+
+    for(i = 0; i < 4; i++) {
+        let info = document.createElement('p');
+        if (i == 0) {
+            info.textContent = book.title;
+        } else if (i == 1) {
+            info.textContent = book.pages;
+        } else if (i == 2) {
+            info.textContent = book.author;
+        } else {
+            info.textContent = book.status;
+        }
+        bookData.appendChild(info);
+    }
+    let removeBook = document.createElement('button');
+    removeBook.textContent = 'Remove book';
+    removeBook.addEventListener('click', () => {
+        shelf.removeChild(bookData);
+        myLibrary.splice(position,1);
+    });
+    bookData.appendChild(removeBook);
+    shelf.appendChild(bookData);
 }
 
 newBook.addEventListener('click', () => {
-    form.style.setProperty('display', 'block');
+    if (myLibrary.length < 10) {
+        form.style.setProperty('display', 'block');    
+    } else {
+        alert('There is a limit of 10 books in this shelf. Please remove at least one in order to add a new book.');
+    }
 });
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let title = document.querySelector('.title');
-    let pages = document.querySelector('.pages');
-    let author = document.querySelector('.author');
+    let title = document.querySelector('.title').value;
+    let pages = document.querySelector('.pages').value;
+    let author = document.querySelector('.author').value;
     let status;
     let yes = document.querySelector('.yes');
     let no = document.querySelector('.no');
@@ -36,7 +75,7 @@ form.addEventListener('submit', (e) => {
         status = 'no';
     }
 
-    addBook(title.value,pages.value,author.value,status);
+    addBook(title,pages,author,status);
 
     form.reset();
     form.style.setProperty('display', 'none');
